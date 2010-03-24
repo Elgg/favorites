e2e8bf1e08ecc9f4fe2b691c939530284ebfa96c
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-24 13:34:19 +0000

    Update to Favorites. File-type icons added for favorite listings.

--- views/default/favorites/css.php
+++ views/default/favorites/css.php
@@ -30,7 +30,6 @@
 	background-position: left -56px;
 }
 
-
 /* favorites listings in sidebar */
 .favorite_list_item {
 	margin:0 0 5px 0;
@@ -41,6 +40,39 @@
 	background:transparent url(<?php echo $vars['url']; ?>mod/favorites/graphics/icons_favorites.png) no-repeat scroll -40px -89px;
 	padding-left:18px;
 }
+
+/* file-type favorites listings in sidebar */
+.favorite_list_item a.application {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/application.png) no-repeat 0 0;
+}
+.favorite_list_item a.archive {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/archive.png) no-repeat 0 0;
+}
+.favorite_list_item a.audio {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/audio.png) no-repeat 0 0;
+}
+.favorite_list_item a.document {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/document.png) no-repeat 0 0;
+}
+.favorite_list_item a.excel {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/excel.png) no-repeat 0 0;
+}
+.favorite_list_item a.general {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/general.png) no-repeat 0 0;
+}
+.favorite_list_item a.image {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/image.png) no-repeat 0 0;
+}
+.favorite_list_item a.pdf {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/pdf.png) no-repeat 0 0;
+}
+.favorite_list_item a.ppt {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/ppt.png) no-repeat 0 0;
+}
+.favorite_list_item a.video {
+	background: url(<?php echo $vars['url']; ?>mod/favorites/graphics/file_types/video.png) no-repeat 0 0;
+}
+
 
 
 /* favorites within plugins */
